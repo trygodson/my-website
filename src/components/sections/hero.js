884cb85ab7f1c6ddaab7d1f013cb2d7caa1b1b47
Godson <gnqnpn@gmail.com>
2023-11-08 00:29:48 +0100

    some changes

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,7 +60,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Godson.</h2>;
-  const three = <h3 className="big-heading">I'm a Front-End Engineer</h3>;
+  const three = <h3 className="big-heading">I'm a Full Stack Engineer</h3>;
   const four = (
     <>
       <p>
